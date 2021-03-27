af61679645c589aac06d453e4b7cdbe171fb5a42
trannhung27 <nt3118274@gmail.com>
2021-03-27 23:56:35 +0700

    upload

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -16,23 +16,6 @@ class AdminController extends Controller
         return view("admin.home");
     }
 
-    public function addUser(Request $request){
-
-        // $name = $request->input('name');
-        // $email = $request->input('email');
-        // $phone = $request->input('phone');
-        // $password =bcrypt($request->input('password'));
-        // $doituong = "reader";
-
-        // $data = array('name'=>$name, "phone"=>$phone, "email"=>$email, "password"=>$password, "doituong"=>$doituong);
-        // $insertid = DB::table('users')->insertGetId($data);
-        // return $insertid;
-        $iduser = $request->input('iduser');
-        $oldpass = $request->input('oldpass');
-        $newpass = $request->input('newpass');
-
-    }
-
     public function changepass(Request $request){
         $iduser = $request->input('iduser');
         $oldpass = $request->input('oldpass');
@@ -47,28 +30,6 @@ class AdminController extends Controller
             return response()->json([[0]]);
         }
     }
-
-
-    public function hu($id)
-    {
-        return view("admin.user");
-    }
-
-    public function hahah(Request $request,$id){
-        // $user = new User();
-        // $user->name = $request->tendangnhap;
-        // $user->email= $request->email;
-        // // $user->phone = $request->sodienthoai;
-        // // $user->password = bcrypt($request->matkhau);
-        // $user->doituong = 'reader';
-
-        $user = User::find($id);
-        if (Hash::check($request->old_password, $user->password)) { 
-            $user->password = bcrypt($request->matkhau);
-            $user->save();
-        }
-    }
-
     
 }
 
